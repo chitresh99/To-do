7e16afaf9a676d89feeaa6f535a4c3b9d414f67a
chitresh99 <chitreshpoojary990@gmail.com>
2024-07-25 00:05:03 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+struct Task {
+    description: String,
+    done: bool,
+}
+
+impl Task {
+    fn new(description: &str) -> Task {
+        Task {
+            description: description.to_string(),
+            done: false,
+        }
+    }
+}
+
+struct TodoApp {
+    tasks: Vec<Task>,
+}
+
+impl TodoApp {
+    fn new() -> TodoApp {
+        TodoApp {
+            tasks: Vec::new(),
+        }
+    }
+
+    // Adding tasks
+    fn add_new_task(&mut self, description: &str) {
+        let task = Task::new(description);
+        self.tasks.push(task);
+    }
+
+    // Displaying tasks
+    fn show_tasks(&self) {
+        for (index, task) in self.tasks.iter().enumerate() {
+            let status = if task.done { "[X]" } else { "[ ]" };
+            println!("{} {} : {}", index + 1, task.description, status);
+        }
+    }
+}
+
+fn main() {
+    let mut app = TodoApp::new();
+    
+    app.add_new_task("Learn Rust");
+    app.add_new_task("Build a project");
+    
+    app.show_tasks();
+}
+
+
